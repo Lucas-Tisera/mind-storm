d7d5cf3076453881d6af1766c9a76b33a99bc8b4
Lucas <luc.tisera.02@gmail.com>
2024-12-10 21:13:13 -0300

    resolve bugs with window undefined in server

--- app/hooks/useScroll.ts
+++ app/hooks/useScroll.ts
@@ -1,25 +1,32 @@
-import { useState, useLayoutEffect } from 'react';
+import { useState, useLayoutEffect } from "react";
 
 export const useScroll = () => {
   const [position, setPosition] = useState({
     x: 0,
-    y: 0
+    y: 0,
   });
 
   const handleScroll = () => {
-    setPosition({
-      x: window.scrollX,
-      y: window.scrollY
-    });
+    if (typeof window !== "undefined") {
+      setPosition({
+        x: window.scrollX,
+        y: window.scrollY,
+      });
+    }
   };
 
   useLayoutEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
   }, []);
 
-  return { position, scrollTo: window.scrollTo };
+  return {
+    position,
+    scrollTo: typeof window !== "undefined" ? window.scrollTo : () => {},
+  };
 };
